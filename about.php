f6fa7fdd4a6fd92bcf2114e804641d69442a990e
unknown <raflirakhapratama@gmail.com>
2023-09-14 20:06:40 +0700

    web travel

---
+++ about.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html><html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Rafli Tours/Travel Tentang</title>
+<!--
+
+Template 2085 Neuron
+
+http://www.tooplate.com/view/2085-neuron
+
+-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Lora|Merriweather:300,400" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="index.html" class="navbar-brand">Rafli Tours/Travel</a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">Beranda</a></li>
+                    <li class="active"><a href="about.html">Tentang</a></li>
+                    <li><a href="gallery.html">Galeri</a></li>
+                    <li><a href="contact.html">Kontak</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+<!-- Home Section -->
+
+<section id="home" class="main-about parallax-section">
+     <div class="overlay"></div>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <h1>Tentang</h1>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- About Section -->
+
+<section id="about">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <div class="col-md-20 col-sm-20">
+                         <h2>Paket Wisata</h2>
+                         <p>Kami menawarkan paket wisata yang menarik dan tentunya membuat konsumen puas terhadap pelayanan kami. Diantaranya : </p> 
+                         <p>1. Paket Wisata Dalam Bandung (Minimal 1 Hari)</p>
+                     	 <p>2. Paket Wisata Luar Bandung (DKI Jakarta, Cianjur, Sukabumi) (Minimal 1 Hari)</p>
+                     	 <p>3. Paket Wisata Luar Bandung (Banten, Pangandaran) (Minimal 2 Hari)</p>
+                     	 <p>4. Paket Wisata Area Jawa Tengah/Yogyakarta (Minimal 3 Hari)</p>
+                     	 <p>5. Paket Wisata Area Jawa Timur (Minimal 5 Hari)</p>
+                     	 <p>6. Paket Wisata Area Luar Jawa (Bali, Lombok) (Minimal 10 Hari)</p>
+                    </div>
+
+                    <div class="col-md-20 col-sm-20">
+                         <h2>Armada Bus</h2>
+                         <p>Kami Menawarkan Armada Bus yang Nyaman dan Aman untuk berwisata kemana saja. Diantaranya : </p>
+                         <p>1. Hino Dutro (17 Kursi)</p>
+                     	 <p>2. Medium (29 Kursi)</p>
+                     	 <p>3. HD (45/47/52/59 Kursi)</p>
+                     	 <p>4. HDD (45/47/52/59 Kursi)</p>
+                     	 <p>5. SHD (45/47/52/59 Kursi)</p>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="col-md-24 col-sm-24">
+                    	 <h3>1. Hino Dutro</h3>
+                         <img src="images/about-image4.jpg" class="img-responsive" alt="Blog Image">
+                         <p>Fasilitas : </p>
+                         <p>1. AC  </p>
+                         <p>2. Reclining Seat Konfigurasi 2-1 </p>
+                         <p>3. TV LCD </p>
+                         <p>4. Karaoke </p>
+                         <p>5. Audio Video </p>
+                    </div>
+
+
+
+                    <div class="col-md-30 col-sm-30">
+                    	 <h3>2. Medium Bus</h3>
+                         <img src="images/about-image5.jpg" class="img-responsive" alt="Blog Image">
+                         <p>Fasilitas : </p>
+                         <p>1. AC  </p>
+                         <p>2. Reclining Seat Konfigurasi 2-2 </p>
+                         <p>3. TV LCD </p>
+                         <p>4. Karaoke </p>
+                         <p>5. Audio Video </p>
+                    </div>
+
+
+
+                    <div class="col-md-24 col-sm-24">
+                    	 <h3>3. Big Bus HD</h3>
+                         <img src="images/about-image1.jpg" class="img-responsive" alt="Blog Image">
+                         <p>Fasilitas : </p>
+                         <p>1. AC  </p>
+                         <p>2. Reclining Seat Konfigurasi 2-2 atau 2-3 </p>
+                         <p>3. TV LCD </p>
+                         <p>4. Karaoke </p>
+                         <p>5. Audio Video </p>
+                    </div>
+
+
+
+                    <div class="col-md-25 col-sm-25">
+                    	 <h3>4. HDD</h3>
+                         <img src="images/about-image2.jpg" class="img-responsive" alt="Blog Image">
+                         <p>Fasilitas : </p>
+                         <p>1. AC  </p>
+                         <p>2. Reclining Seat Konfigurasi 2-2 atau 2-3 </p>
+                         <p>3. TV LCD </p>
+                         <p>4. Karaoke </p>
+                         <p>5. Audio Video </p>
+                    </div>
+
+
+
+                    <div class="col-md-36 col-sm-36">
+                    	 <h3>5. SHD</h3>
+                         <img src="images/about-image3.jpg" class="img-responsive" alt="Blog Image">
+                         <p></p>
+                         <p>Fasilitas : </p>
+                         <p>1. AC  </p>
+                         <p>2. Reclining Seat Konfigurasi 2-2 atau 2-3 </p>
+                         <p>3. TV LCD </p>
+                         <p>4. Karaoke </p>
+                         <p>5. Audio Video </p>
+                    </div>
+
+                    <div class="col-md-36 col-sm-36">
+                    	 <h3></h3>
+					</div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Footer Section -->
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-5 col-md-offset-1 col-sm-6">
+                    <h3>Rafli Tours/Travel</h3>
+                    <p>Terimakasih telah mempercayai kami sebagai agen anda, untuk kritik dan saran silahkan kirimkan melalui email sebagai motivasi untuk kami kedepannya nanti.</p>
+                    <div class="footer-copyright">
+                         <p>Copyright &copy; 2019 Rafli Tours/Travel Company</p>
+                    	<p>Hak Cipta Dilindungi</p>
+                    </div>
+               </div>
+
+               <div class="col-md-4 col-md-offset-1 col-sm-6">
+                    <h3>Kontak dan Alamat</h3>
+                    <p><i class="fa fa-globe"></i> Jalan Manisi (Muda Mandiri Center) No. A18 Desa Mekarsari, Kecamatan Cibiru, Kota Bandung, Jawa Barat</p>
+                    <p><i class="fa fa-whatsapp"></i> 083112650936</p>
+                    <p><i class="fa fa-instagram"></i> rrakhapratama</p>
+                    <p><i class="fa fa-facebook"></i> Rafli Rakha Pratama</p>
+              		<p><i class="fa fa-google"></i> raflirakhapratama@gmail.com</p>
+                </div>
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+
+               <div class="col-md-12 col-sm-12">
+                    <ul class="social-icon">
+                         <li><a href="https://www.facebook.com/rafli.r.pratama.1" class="fa fa-facebook"></a></li>
+                         <li><a href="https://www.instagram.com/rrakhapratama/" class="fa fa-instagram"></a></li>
+                         <li><a href="whatsapp://send?text=Hello, saya mau menanyakan atau reservasi&phone=+6283112650936" class="fa fa-whatsapp"></a></li></
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Rafli Tours/Travel Kontak</title>
+<!--
+
+Template 2085 Neuron
+
+http://www.tooplate.com/view/2085-neuron
+
+-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Lora|Merriweather:300,400" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="index.html" class="navbar-brand">Rafli Tours/Travel</a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">Beranda</a></li>
+                    <li><a href="about.html">Tentang</a></li>
+                    <li><a href="gallery.html">Galeri</a></li>
+                    <li class="active"><a href="contact.html">Kontak</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+<!-- Home Section -->
+
+<section id="home" class="main-contact parallax-section">
+     <div class="overlay"></div>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <h1>Kontak</h1>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Contact Section -->
+
+<section id="contact">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <h2>Kontak Pemesanan</h2>
+                    <p>Untuk pemesanan dan reservasi silahkan hubungi atau kirim via email:</p>
+
+                     <li><a href="whatsapp://send?text=Hello, saya mau menanyakan atau reservasi&phone=+6283112650936" class="fa fa-whatsapp"> 083112650936 (Rafli Rakha Pratama)</a></li>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Footer Section -->
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-5 col-md-offset-1 col-sm-6">
+                    <h3>Rafli Tours/Travel</h3>
+                    <p>Terimakasih telah mempercayai kami sebagai agen anda, untuk kritik dan saran silahkan kirimkan melalui email sebagai motivasi untuk kami kedepannya nanti.</p>
+                    <div class="footer-copyright">
+                         <p>Copyright &copy; 2019 Rafli Tours/Travel Company</p>
+                         <p>Hak Cipta Dilindungi</p>
+                    </div>
+               </div>
+
+              <div class="col-md-4 col-md-offset-1 col-sm-6">
+                    <h3>Kontak dan Alamat</h3>
+                    <p><i class="fa fa-globe"></i> Jalan Manisi (Muda Mandiri Center) No. A18 Desa Mekarsari, Kecamatan Cibiru, Kota Bandung, Jawa Barat</p>
+                    <p><i class="fa fa-whatsapp"></i> 083112650936</p>
+                    <p><i class="fa fa-instagram"></i> rrakhapratama</p>
+                    <p><i class="fa fa-facebook"></i> Rafli Rakha Pratama</p>
+                    <p><i class="fa fa-google"></i> raflirakhapratama@gmail.com</p>
+               </div>
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+
+              <div class="col-md-12 col-sm-12">
+                    <ul class="social-icon">
+                         <li><a href="https://www.facebook.com/rafli.r.pratama.1" class="fa fa-facebook"></a></li>
+                         <li><a href="https://www.instagram.com/rrakhapratama/" class="fa fa-instagram"></a></li>
+                        <li><a href="whatsapp://send?text=Hello, saya mau menanyakan atau reservasi&phone=+6283112650936" class="fa fa-whatsapp"></a></li>
+                    </ul>
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
